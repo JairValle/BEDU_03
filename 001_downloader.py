20c7a1a718474667ee2ae2f007f129033659d3db
Jairvalle <jairvallehdz@gmail.com>
2020-08-22 12:31:07 -0500

    Read csv

--- 001_downloader.py
+++ 001_downloader.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 
 # Direcciones para consultar información segun criterios https://api.github.com/users/JairValle
@@ -5,6 +6,8 @@ import requests
 # CONSTANTS
 URL_BASE='https://api.github.com/'
 
+
+# FUNTIONS
 def get_github_user(username):
     url = f'{URL_BASE}users/{username}'
     response = requests.get(url)
@@ -12,16 +15,28 @@ def get_github_user(username):
         return response.json()
     return None
 
+
 def download_user_avatar(image_url):
     response = requests.get(image_url)
-    if response.status.code == 200:
-        print(response.content)
+    if response.status_code == 200:
+        response_content = response.content
+        filename = f'tmp/{image_filename()}.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
     return None
 
-# username = input('Give the username:\t')
-# user = get_github_user(username)
+
+def image_filename():
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    return timestamp
 
 
-# print(user.get('avatar_url'))
-
-download_user_avatar('https://avatars2.githubusercontent.com/u/67514107?v=4')
+username = input('Give the username:\t')
+user = get_github_user(username)
+if user:
+    user_avatar_url = user.get('avatar_url')
+    download_user_avatar(user_avatar_url)
+else:
+    print('User not found')
