521f7af2509b8b9bad133552b0db0d000824e7d5
Jairvalle <jairvallehdz@gmail.com>
2020-08-22 13:20:39 -0500

    writerrandomm csv

--- writer003_csv.py
+++ writer003_csv.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 
 # CONSTANT
@@ -7,7 +8,13 @@ COLUMN_NAMES = ['employee_id', 'entered_at', 'enter_for']
 MAX_NUMBER_LOGS = 100
 
 
-with open(FILENAME, mode='w', newline = '') as csv_file:
+# FUNCTION
+def fake_enter_for():
+    enter_for_list = ['suppliement', 'reports', 'meeting', 'payment', 'bathroom', 'call', 'lunch']
+    return random.choice(enter_for_list)
+
+
+with open(FILENAME, mode='w', newline='') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=COLUMN_NAMES)
     # WRITING HEADERS
     writer.writeheader()
@@ -16,6 +23,7 @@ with open(FILENAME, mode='w', newline = '') as csv_file:
         writer.writerow({
             'employee_id': 1002,
             'entered_at': 716253576253,
-            'enter_for': 'LUNCH'
+            'enter_for': fake_enter_for()
         })
         counter += 1
+
